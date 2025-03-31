1660f09690a9c9e0e2c0b334346540aebb8d7a02
tobias ferreira <soutobias@yahoo.com.br>
2025-03-31 14:10:30 +0100

    update remote sensing data for altimeter and scaterometer

--- oceanobs/remote_sensing/scatterometer.py
+++ oceanobs/remote_sensing/scatterometer.py
@@ -1,125 +1,167 @@
+from concurrent.futures import ThreadPoolExecutor, as_completed
 import os
 import tempfile
 from datetime import datetime, timedelta
 
 import numpy as np
 import pandas as pd
+from tqdm import tqdm
 import xarray as xr
 from dotenv import load_dotenv
 from harmony import BBox, Client, Collection, Environment, LinkType, Request
 
+from oceanobs.oceanobs import Oceanobs
 from oceanobs.oceanobs_handler.db_handler import DbHandler
 
+load_dotenv()
 
-class Metop:
-    load_dotenv()
+class Scatterometer(Oceanobs):
+    """ Scatterometer data collection class
 
-    def __init__(
-        self,
-        b_box=(-55, -35, 20, 7),
-        step=20,
-        datasets=["C2075141559-POCLOUD"],
-        start_date=datetime.utcnow() - timedelta(days=2),
-        end_date=datetime.utcnow() + timedelta(days=1),
-    ):
-        self.db = DbHandler()
-        self.b_box = BBox(b_box[0], b_box[1], b_box[2], b_box[3])
-        self.step = step
-        self.start_date = start_date
-        self.end_date = end_date
 
-        self.datasets = datasets
+    Parameters
+    ----------
+    start_date : str, optional
+        Start date for the data collection, by default None
+    end_date : str, optional
+    lat_lon_limits : dict, optional
+        Latitude and longitude limits, by default None
+    n_workers : int, optional
+        Number of workers for the thread pool executor, by default 1
+    """
+    def __init__(self,
+                 collections=["C2075141559-POCLOUD"],
+                 start_date: str = None,
+                 end_date: str = None,
+                 lat_lon_limits: dict = None,
+                 n_workers: int = 1,
+                 **kwargs):
 
-        self.username = os.getenv("EDL_USR")
-        self.password = os.getenv("EDL_PWD")
+        super().__init__(start_date=start_date,
+                         end_date=end_date,
+                         lat_lon_limits=lat_lon_limits,
+                         n_workers=n_workers)
+        self.start_date = self._validate_date(start_date, is_start_date=True)
+        self.end_date = self._validate_date(end_date, is_start_date=False)
+        self.collections = collections
+        self._username = os.getenv("EDL_USR")
+        self._password = os.getenv("EDL_PWD")
+        if not self._username or not self._password:
+            raise ValueError("Username and password must be set")
 
-    def get(self):
-        wind_flag = pd.DataFrame([], dtype="float64")
-        wind_dir = pd.DataFrame([], dtype="float64")
-        wind_speed = pd.DataFrame([], dtype="float64")
-        wind_time = pd.DataFrame([], dtype="float64")
-        wind_lat = pd.DataFrame([], dtype="float64")
-        wind_lon = pd.DataFrame([], dtype="float64")
+    def get(self) -> pd.DataFrame:
+        """Get the data from Scatterometers
 
-        for collection in self.datasets:
-            self.download(collection)
-            for f in self.nc_files:
-                print(f)
-                ds = xr.open_dataset(f)
-                wind_flag = pd.concat(
-                    [wind_flag, pd.DataFrame(ds["wvc_quality_flag"].values)], axis=0
-                )
-                wind_dir = pd.concat(
-                    [wind_dir, pd.DataFrame(ds["wind_dir"].values)], axis=0
-                )
-                wind_speed = pd.concat(
-                    [wind_speed, pd.DataFrame(ds["wind_speed"].values)], axis=0
-                )
-                wind_time = pd.concat(
-                    [wind_time, pd.DataFrame(ds["time"].values)], axis=0
-                )
-                wind_lat = pd.concat([wind_lat, pd.DataFrame(ds["lat"].values)], axis=0)
-                wind_lon = pd.concat([wind_lon, pd.DataFrame(ds["lon"].values)], axis=0)
-                os.remove(f)
+        Returns
+        -------
+        pd.DataFrame
+            DataFrame with the data
+        """
+        wind_flag, wind_dir, wind_speed, wind_time, wind_lat, wind_lon = [], [], [], [], [], []
+        for collection in self.collections:
+            nc_files = self.download(collection)
+            with ThreadPoolExecutor(max_workers=self.n_workers) as executor:
+                futures = [
+                    executor.submit(self.get_data, file=file, wind_flag=wind_flag, wind_dir=wind_dir, wind_speed=wind_speed, wind_time=wind_time, wind_lat=wind_lat, wind_lon=wind_lon)
+                    for file in nc_files
+                ]
+                for future in tqdm(as_completed(futures), desc="Downloading data", total=len(futures)):
+                    future.result()
+        date_time = np.concatenate(wind_time)
+        wdir = np.concatenate(wind_dir)
+        wspd = np.concatenate(wind_speed)
+        lat = np.concatenate(wind_lat)
+        lon = np.concatenate(wind_lon)
+        win_flag = np.concatenate(wind_flag)
 
-        date_time = np.array(pd.melt(wind_time, value_name="date_time")["date_time"])
-        wdir = np.array(pd.melt(wind_dir, value_name="wdir")["wdir"])
-        wspd = np.array(pd.melt(wind_speed, value_name="wspd")["wspd"])
-        lat = np.array(pd.melt(wind_lat, value_name="lat")["lat"])
-        lon = np.array(pd.melt(wind_lon, value_name="lon")["lon"])
+        # Create final DataFrame
+        data = pd.DataFrame({
+            "date_time": date_time,
+            "latitude": lat,
+            "longitude": lon,
+            "wdir": wdir,
+            "wspd": wspd,
+            "flag": win_flag,
+        })
+        data = data.loc[data["date_time"].notna()]
+        data = data.loc[data["latitude"].notna()]
+        data = data.loc[data["longitude"].notna()]
+        data = data.loc[data["flag"] == 0]
+        data.drop(columns="flag", inplace=True)
+        data.wspd = (data.wspd * 1.94384).round(decimals=1)
+        data = data.replace(0, np.nan)
+        data = self.convert_to_numeric(data)
+        data["longitude"] = data["longitude"].apply(lambda x: x - 180 if x > 180 else x)
+        data = self._convert_to_gdf(data)
+        data["station_type"] = "Scatterometer"
+        return data
 
-        win_flag = np.array(pd.melt(wind_flag, value_name="win_flag")["win_flag"])
-        allData = [date_time, lat, lon, wdir, wspd, win_flag]
 
-        df = pd.DataFrame(allData).T
-        df.columns = ["date_time", "lat", "lon", "wdir", "wspd", "flag"]
-        df = df[df["flag"] == 0]
-        df.lat = pd.to_numeric(df.lat, errors="coerce").round(decimals=4)
-        df.lon = pd.to_numeric(df.lon, errors="coerce").round(decimals=4)
-        df.wdir = pd.to_numeric(df.wdir, errors="coerce").round()
-        df.wspd = pd.to_numeric(df.wspd, errors="coerce").round(decimals=1)
-        df = df.replace(0, np.nan)
-        df.drop(columns="flag", inplace=True)
-        df.dropna(inplace=True)
-        df.lon = df.lon - 360
-        df = df.iloc[:: self.step]
+    def get_data(self, file: str, wind_flag: list, wind_dir: list, wind_speed: list, wind_time: list, wind_lat: list, wind_lon: list):
+        """Get the data from the Scatterometer
 
-        self.result = df
-        print("Verificando se há dados")
-        if not self.result.empty:
-            print("Há dados")
+        Parameters
+        ----------
+        file : str
+            URL of the file to download
+        wind_flag : list
+            List of wind quality flags
+        wind_dir : list
+            List of wind directions
+        wind_speed : list
+            List of wind speeds
+        wind_time : list
+            List of wind times
+        wind_lat : list
+            List of wind latitudes
+        wind_lon : list
+            List of wind longitudes
+        """
+        ds = xr.open_dataset(file)
+        wind_flag.append(ds["wvc_quality_flag"].values.ravel())
+        wind_dir.append(ds["wind_dir"].values.ravel())
+        wind_speed.append(ds["wind_speed"].values.ravel())
+        wind_time.append(ds["time"].values.ravel())
+        wind_lat.append(ds["lat"].values.ravel())
+        wind_lon.append(ds["lon"].values.ravel())
+        os.remove(file)
 
-            self.result["institution"] = "metop"
-            self.result["data_type"] = "scatterometer"
+    def _validate_date(self, date_str: str = None, is_start_date: bool = True) -> str:
+        """Validates the date format and returns a datetime object.
 
-            self.result.wspd = (self.result.wspd * 1.94384).round(decimals=1)
+        Parameters
+        ----------
+        date_str : str, optional
+            Date string to validate, by default None
+        is_start_date : bool, optional
+            If the date is the start date, by default True
 
-            self.db.feed_bd(
-                table="data_no_stations", df=self.result, data_type="scatterometer"
-            )
-        else:
-            print("Não há dados")
+        Returns
+        -------
+        str
+            The validated date
+        """
+        date_str = super()._validate_date(date_str, is_start_date)
+        date_str = datetime.strptime(date_str, "%Y-%m-%dT%H:%M:%S")
+        return date_str
 
     def download(self, collection):
-        harmony_client = Client(auth=(self.username, self.password))
+        harmony_client = Client(auth=(self._username, self._password))
         collection_id = Collection(collection)
 
         request = Request(
             collection=collection_id,
             temporal={"start": self.start_date, "stop": self.end_date},
-            spatial=BBox(-55, -35, 20, 7),  #  lat: (-45.75:45), lon: (-90:90)
+            spatial=BBox(self.lat_lon_limits["min_lon"], self.lat_lon_limits["min_lat"], self.lat_lon_limits["max_lon"], self.lat_lon_limits["max_lat"]),
         )
         if request.is_valid():
             job_id = harmony_client.submit(request)
-            print("\n Waiting for the job to finish. . .\n")
-            response = harmony_client.result_json(job_id, show_progress=True)
-            print("\n. . .DONE!")
+            self.logger.info(f"Job ID: {job_id}")
+            harmony_client.result_json(job_id, show_progress=True)
+            self.logger.info(f"Job ID: {job_id} completed")
         temp_dir = tempfile.mkdtemp()
         futures = harmony_client.download_all(
             job_id, directory=temp_dir, overwrite=True
         )
-        self.nc_files = [f.result() for f in futures]
-
-
-if __name__ == "__main__":
-    Metop().get()
+        nc_files = [f.result() for f in futures]
+        return nc_files
